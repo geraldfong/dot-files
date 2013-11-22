851e53c487f1301891140e37c9361edb81d03425
Gerald Fong <geraldgfong@gmail.com>
2013-11-22 19:42:13 +0000

    removing .vim, adding install script

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+git config --global user.name "Gerald Fong"
+git config --global user.email geraldgfong@gmail.com
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
